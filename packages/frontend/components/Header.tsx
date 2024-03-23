04cbc406c9e0dfeedbbc0803c8cb1da661ca7d24
Fu-Chuan Chung <sh1001309@gmail.com>
2024-03-23 13:59:09 +0000

    fix: remove redundant links

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -13,12 +13,6 @@ export const Header = () => {
         <div className="flex justify-between h-20 px-10 shadow items-center">
             <div className="flex items-center space-x-8">
             <h1 className="text-xl lg:text-2xl font-bold cursor-pointer italic text-red-500">FirePost</h1>
-            <div className="hidden md:flex justify-around space-x-4">
-                <a href="#" className="hover:text-indigo-600 text-gray-700">Home</a>
-                <a href="#" className="hover:text-indigo-600 text-gray-700">About</a>
-                <a href="#" className="hover:text-indigo-600 text-gray-700">Service</a>
-                <a href="#" className="hover:text-indigo-600 text-gray-700">Contact</a>
-            </div>
             </div>
             <div className="flex space-x-4 items-center">
             <ConnectButton/> 
